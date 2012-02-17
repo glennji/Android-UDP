99f3d766961b75e39b5b9f6e7d8dcb29e8b516f0
glennji <glenn@glennji.com>
2012-02-18 10:53:49 +1100

    Adds the worlds ugliest UI, separates send and receive threads
    Now tested with sockettest[1]; this is indeed sending "Wake up" payload
    to the UDP socket specified. Now, if only there was an existing user
    interface definition that we could copy-paste this code into ...
    
    [1] http://sockettest.sourceforge.net/

--- Android UDP/src/org/glennji/devcamp/AndroidUDPActivity.java
+++ Android UDP/src/org/glennji/devcamp/AndroidUDPActivity.java
@@ -10,6 +10,7 @@ import java.net.UnknownHostException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.googlecode.androidannotations.annotations.Background;
@@ -22,33 +23,55 @@ import com.googlecode.androidannotations.annotations.ViewById;
 public class AndroidUDPActivity extends Activity {
 
 	@ViewById
-	TextView hello;
+	TextView output;
 
-	int PORT = 12345;
+	@ViewById
+	EditText hostname;
+
+	@ViewById
+	EditText port;
+
 	String data = "Wake up, you!";
-	String host = "10.0.1.129";
 
-	@Click(R.id.button1)
-	void startForkableComputation() {
-		doSomeStuffInBackground();
+	@Click(R.id.sendPacketButton)
+	void clickSendPacketButton() {
+		DatagramSocket socket;
+		try {
+			int portInt = Integer.parseInt(port.getText().toString());
+			if (portInt < 1024) {
+				printOutput("Can't send to a port below 1024");
+				port.setText("1024");
+			}
+			socket = openSocket(portInt);
+			sendPacket(socket, portInt);
+			receiveResponse(socket);
+		} catch (NumberFormatException e) {
+			printOutput("Port must be a number!");
+		}
+	}
+
+	DatagramSocket openSocket(int port) {
+		try {
+			return new DatagramSocket(port);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@Background
-	void doSomeStuffInBackground() {
+	void sendPacket(DatagramSocket socket, int port) {
 		try {
-			InetAddress address = InetAddress.getByName(host);
-			DatagramSocket socket = new DatagramSocket(PORT);
+			InetAddress address = InetAddress.getByName(hostname.getText()
+					.toString());
+
 			DatagramPacket packet = new DatagramPacket(data.getBytes(),
-					data.length(), address, PORT);
+					data.length(), address, port);
 			socket.send(packet);
 			Log.d("AndroidUDP", "Sent: " + packet);
-			publishProgress("Packet sent");
-			byte[] buf = new byte[1024];
-			DatagramPacket inputPacket = new DatagramPacket(buf, buf.length);
-			socket.receive(inputPacket);
-			String s = new String(packet.getData(), 0, packet.getLength());
-			Log.d("AndroidUDP", "Received: " + s);
-			publishProgress(s);
+			printOutput("Packet sent, waiting for response");
+
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -61,9 +84,31 @@ public class AndroidUDPActivity extends Activity {
 		}
 	}
 
+	@Background
+	void receiveResponse(DatagramSocket socket) {
+		try {
+			byte[] buf = new byte[1024];
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+			socket.setSoTimeout(3);
+			socket.receive(packet);
+			if (packet.getData().length < 1) {
+				printOutput("Timed out with no response");
+			} else {
+				String s = new String(packet.getData(), 0, packet.getLength());
+				Log.d("AndroidUDP", "Received: " + s);
+				printOutput(s);
+			}
+			socket.close();
+		} catch (SocketException sex) {
+			printOutput("Socket exception! " + sex.getMessage());
+		} catch (IOException ioex) {
+			printOutput("IOException! " + ioex.getMessage());
+		}
+	}
+
 	@UiThread
-	void publishProgress(String update) {
-		hello.setText(update);
+	void printOutput(String update) {
+		output.append("\n" + update);
 	}
 
 }
