06e1116fb1f233c2f95df2f3d34a3e52dcc00443
Tarcio Saraiva <tarcio@gmail.com>
2012-02-20 20:35:48 +1100

    Sexy it up a bit, fix libs and add a read me file

--- Android UDP/src/org/glennji/devcamp/AndroidUDPActivity.java
+++ Android UDP/src/org/glennji/devcamp/AndroidUDPActivity.java
@@ -8,7 +8,6 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,93 +21,92 @@ import com.googlecode.androidannotations.annotations.ViewById;
 @EActivity(R.layout.main)
 public class AndroidUDPActivity extends Activity {
 
-	@ViewById
-	TextView output;
+  @ViewById
+  TextView output;
 
-	@ViewById
-	EditText hostname;
+  @ViewById
+  EditText hostname;
 
-	@ViewById
-	EditText port;
+  @ViewById
+  EditText port;
 
-	String data = "Wake up, you!";
+  String data = "Wake up, you!";
 
-	@Click(R.id.sendPacketButton)
-	void clickSendPacketButton() {
-		DatagramSocket socket;
-		try {
-			int portInt = Integer.parseInt(port.getText().toString());
-			if (portInt < 1024) {
-				printOutput("Can't send to a port below 1024");
-				port.setText("1024");
-			}
-			socket = openSocket(portInt);
-			sendPacket(socket, portInt);
-			receiveResponse(socket);
-		} catch (NumberFormatException e) {
-			printOutput("Port must be a number!");
-		}
-	}
+  @Click(R.id.sendPacketButton)
+  void clickSendPacketButton() {
+    DatagramSocket socket;
+    try {
+      final int portInt = Integer.parseInt(port.getText().toString());
+      if (portInt < 1024) {
+        printOutput("Can't send to a port below 1024");
+        port.setText("1024");
+      }
+      socket = openSocket(portInt);
+      sendPacket(socket, portInt);
+      receiveResponse(socket);
+    } catch (final NumberFormatException e) {
+      printOutput("Port must be a number!");
+    }
+  }
 
-	DatagramSocket openSocket(int port) {
-		try {
-			return new DatagramSocket(port);
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+  DatagramSocket openSocket(final int port) {
+    try {
+      return new DatagramSocket(port);
+    } catch (final SocketException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return null;
+  }
 
-	@Background
-	void sendPacket(DatagramSocket socket, int port) {
-		try {
-			InetAddress address = InetAddress.getByName(hostname.getText()
-					.toString());
+  @Background
+  void sendPacket(final DatagramSocket socket, final int port) {
+    try {
+      final InetAddress address = InetAddress.getByName(hostname.getText().toString());
 
-			DatagramPacket packet = new DatagramPacket(data.getBytes(),
-					data.length(), address, port);
-			socket.send(packet);
-			Log.d("AndroidUDP", "Sent: " + packet);
-			printOutput("Packet sent, waiting for response");
+      final DatagramPacket packet = new DatagramPacket(data.getBytes(), data.length(), address,
+          port);
+      socket.send(packet);
+      Log.d("AndroidUDP", "Sent: " + packet);
+      printOutput("Packet sent, waiting for response");
 
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    } catch (final UnknownHostException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (final SocketException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (final IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
-	@Background
-	void receiveResponse(DatagramSocket socket) {
-		try {
-			byte[] buf = new byte[1024];
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			socket.setSoTimeout(3);
-			socket.receive(packet);
-			if (packet.getData().length < 1) {
-				printOutput("Timed out with no response");
-			} else {
-				String s = new String(packet.getData(), 0, packet.getLength());
-				Log.d("AndroidUDP", "Received: " + s);
-				printOutput(s);
-			}
-			socket.close();
-		} catch (SocketException sex) {
-			printOutput("Socket exception! " + sex.getMessage());
-		} catch (IOException ioex) {
-			printOutput("IOException! " + ioex.getMessage());
-		}
-	}
+  @Background
+  void receiveResponse(final DatagramSocket socket) {
+    try {
+      final byte[] buf = new byte[1024];
+      final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+      socket.setSoTimeout(3);
+      socket.receive(packet);
+      if (packet.getData().length < 1) {
+        printOutput("Timed out with no response");
+      } else {
+        final String s = new String(packet.getData(), 0, packet.getLength());
+        Log.d("AndroidUDP", "Received: " + s);
+        printOutput(s);
+      }
+      socket.close();
+    } catch (final SocketException sex) {
+      printOutput("Socket exception! " + sex.getMessage());
+    } catch (final IOException ioex) {
+      printOutput("IOException! " + ioex.getMessage());
+    }
+  }
 
-	@UiThread
-	void printOutput(String update) {
-		output.append("\n" + update);
-	}
+  @UiThread
+  void printOutput(final String update) {
+    output.append("\n" + update);
+  }
 
 }
